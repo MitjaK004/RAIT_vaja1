ad2090fc87e725ecd85588c24483e92983ac2a77
MitjaK004 <smetar123456789@gmail.com>
2025-03-11 15:07:31 +0100

    Initial commit

---
+++ controllers/articles_controller.php
@@ -0,0 +1,60 @@
+<?php
+/*
+    Controller za novice. Vključuje naslednje standardne akcije:
+        index: izpiše vse novice
+        show: izpiše posamezno novico
+        
+    TODO:
+        list: izpiše novice prijavljenega uporabnika
+        create: izpiše obrazec za vstavljanje novice
+        store: vstavi novico v bazo
+        edit: izpiše vmesnik za urejanje novice
+        update: posodobi novico v bazi
+        delete: izbriše novico iz baze
+*/
+
+class articles_controller
+{
+    public function index()
+    {
+        //s pomočjo statične metode modela, dobimo seznam vseh novic
+        //$ads bo na voljo v pogledu za vse oglase index.php
+        $articles = Article::all();
+
+        //pogled bo oblikoval seznam vseh oglasov v html kodo
+        require_once('views/articles/index.php');
+    }
+
+    public function show()
+    {
+        //preverimo, če je uporabnik podal informacijo, o oglasu, ki ga želi pogledati
+        if (!isset($_GET['id'])) {
+            return call('pages', 'error'); //če ne, kličemo akcijo napaka na kontrolerju stran
+            //retun smo nastavil za to, da se izvajanje kode v tej akciji ne nadaljuje
+        }
+        //drugače najdemo oglas in ga prikažemo
+        $article = Article::find($_GET['id']);
+        require_once('views/articles/show.php');
+    }
+
+    public function create(){
+        $error = "";
+        if(isset($_GET["error"])){
+            switch($_GET["error"]){
+                case 1: $error = "Izpolnite vse podatke"; break;
+                default: $error = "Prišlo je do napake med registracijo uporabnika.";
+            }
+        }
+        require_once('views/articles/create.php');
+    }
+
+    public function store(){
+        if(!isset($_POST["title"]) || !isset($_POST["abstract"]) || !isset($_POST["text"]) || !isset($_POST["date"]) || !isset($_SESSION["USER_ID"])){
+            header("Location: /articles/create?error=1");
+        }
+        else{
+            $article = Article::create($_POST["title"], $_POST["abstract"], $_POST["text"], $_POST["date"], $_SESSION["USER_ID"]);
+        }
+        header("Location: /");
+    }
+}
